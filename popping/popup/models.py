de38ec3521f32b063b2279a2bd2d95da3c7fafba
Jeongmingz <joungminyt@gmail.com>
2024-08-07 10:57:25 +0900

    popping online popup models

--- popping/popup/models.py
+++ popping/popup/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from share.models import TimeModel
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid
 
-# Create your models here.
+
+# popup Brands
+class Brands(models.Model):
+	manager = models.ForeignKey(
+		'user.User',
+		on_delete=models.DO_NOTHING
+		)
+
+	name = models.TextField(
+		# 브랜드의 이름
+		max_length=200,
+		unique=True,
+		)
+
+	logo = models.URLField(
+		# 브랜드의 로고
+		null=True,
+		blank=True
+		)
+
+	conditions = models.JSONField(
+		# 브랜드 계약 조건
+		# 수수료, 등등
+		default=dict
+		)
+
+	proceeding = models.BooleanField(
+		# 현재 온라인 팝업 진행 여부
+		default=False
+		)
+
+	contractStart = models.DateTimeField(
+		# 브랜드 계약 시작일
+		null=True,
+		blank=True
+		)
+
+	contractEnd = models.DateTimeField(
+		# 브랜드 계약 만료일
+		null=True,
+		blank=True
+		)
+
+# online popup's products
+class Product(TimeModel):
+	pass
+
+# order list
+class Order(TimeModel):
+	pass
+
+# order customer service
+class OrderCS(TimeModel):
+	pass
+
+
+
+
