2784103cbb769e6e426cfe9f5b781cd27719b786
wogml8524 <wogml8524@naver.com>
2024-08-29 04:58:05 +0900

    api, serializer 추가 수정

--- popping/map/apis.py
+++ popping/map/apis.py
@@ -8,28 +8,102 @@ from rest_framework.response import Response
 from rest_framework import status
 from config.settings import MONGO_DB_NAME
 from .models import OfflinePopup, Place
-from .serializers import PlaceSerializer, OfflinePopupStoreSerializer, OfflinePopupStoreSimpleSerializer
+from .serializers import PlaceSerializer, OfflinePopupStoreSerializer, OfflinePopupStoreSimpleSerializer, MainPopupSerializer
 from .mongodb import MongoDBClient
 from tqdm import tqdm
 import json
 from bson import ObjectId
+from datetime import datetime
 
-subway = {
-    "성수역" : [127.055983543396, 37.54457732085582],
-    "강남역" : [127.02761650085449, 37.49796319921411],
-    "잠실역" : [127.10013270378113, 37.5132661890097],
-    "용산역" : [126.96480184793472, 37.52988484762269],
-    "여의도역" : [126.92406177520752, 37.52163980072133],
-    "홍대입구역" : [126.925950050354, 37.55811021038101],
-    "압구정역" : [127.02849626541138, 37.52633678124275],
-    "삼성역" : [127.06318259239197, 37.50887477317293],
-}
+# main page 팝업 리스트들
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def main_popup(request):
+    response_data = {}
+    now = datetime.utcnow()
+    collection = MongoDBClient.get_collection(MONGO_DB_NAME,'OfflinePopup')
+    
+    popularity_pipeline = [
+         {
+            "$match": {
+                "status": 1
+            }
+        },
+        {
+            "$addFields": {
+                "calculated_value": {
+                    "$add": [
+                        {"$multiply": ["$saveCount", 3]},
+                        "$viewCount"
+                    ]
+                }
+            }
+        },
+        {
+            "$match": {
+                "$expr": {
+                    "$and": [
+                        {"$ne": ["$calculated_value", 0]},
+                        {"$ne": ["$calculated_value", None]}
+                    ]
+                }
+            }
+        },
+        {
+            "$sort": {
+                "calculated_value": -1
+            }
+        },
+        {
+            "$limit": 9
+        }
+    ]
+    date_pipeline = [
+        {
+            "$match": {
+                "status": 1
+            }
+        },
+        {
+            "$addFields": {
+                "date_difference": {
+                    "$abs": {
+                        "$subtract": [now, "$date.start"]
+                    }
+                }
+            }
+        },
+        {
+            "$sort": {
+                "date_difference": 1
+            }
+        },
+        {
+            "$limit": 9
+        }
+    ]
 
+    # 집계 실행
+    popularity_query = list(collection.aggregate(popularity_pipeline))
+    date_query = list(collection.aggregate(date_pipeline))
+    
+    popularity_serializer = MainPopupSerializer(popularity_query, many=True)
+    date_serializer = MainPopupSerializer(date_query, many=True)
+    
+    response_data = {
+        'sortPopularity':popularity_serializer.data,
+        'sortDate':date_serializer.data
+    }
+    
+    return Response(response_data, status=status.HTTP_200_OK)
+    
 # 팝업리스트 api
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def offline_popups(request):
     response_data = {}
+    
+    context = {"user": request.user}
     
     sort_option = request.GET.get('sorted')
     district = request.GET.get('district')
@@ -57,12 +131,11 @@ def offline_popups(request):
             )
         # 이건 api 호출보단 front에서 정렬하는게 나을듯???
         case "popularity":
-            popupStore_query = popupStore_query.order_by('-viewCount')
+            popupStore_query = popupStore_query.order_by('-viewCount')[:10]
         
         case _:
             pass
     
-    context = {"user": request.user}
     serializer = OfflinePopupStoreSimpleSerializer(popupStore_query, many=True, context=context)
     
     response_data = {
@@ -70,40 +143,6 @@ def offline_popups(request):
     }
     
     return Response(response_data, status=status.HTTP_200_OK)
-
-# 팝업 조회 api
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def popup_detail(request, popupId):
-    response_data = {}
-    
-    context = {"user": request.user}
-    
-    popupStore_query = OfflinePopup.objects.get(id=popupId)
-    serializer = OfflinePopupStoreSerializer(popupStore_query, context=context)
-    response_data = {
-        'popupData': serializer.data
-    }
-    return Response(response_data, status=status.HTTP_200_OK)
-
-# 조회시 count api
-@api_view(['GET'])
-@permission_classes([AllowAny])
-def count_view(request, popupId):
-    
-    option = request.GET.get('option')
-    
-    match option:
-        
-        case "popup":
-            data_query = OfflinePopup.objects.get(id=popupId)
-        case "place":
-            data_query = Place.objects.get(id=popupId)
-            
-    data_query.viewCount = data_query.viewCount + 1
-    data_query.save()  # 변경된 내용을 저장
-    
-    return Response(status=status.HTTP_200_OK)
 
 # 위치 중심 팝업 리스트 조회
 @api_view(['GET'])
@@ -150,7 +189,6 @@ def surround_popup(request):
     
     return Response(response_data, status=status.HTTP_200_OK)
 
-
 # 팝업 중심 place 조회 api
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -185,6 +223,42 @@ def surround_place(request):
     }
                 
     return Response(response_data, status=status.HTTP_200_OK)
+
+# 팝업 조회 api
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def popup_detail(request, popupId):
+    response_data = {}
+    
+    context = {"user": request.user}
+    
+    popupStore_query = OfflinePopup.objects.get(id=popupId)
+    serializer = OfflinePopupStoreSerializer(popupStore_query, context=context)
+    response_data = {
+        'popupData': serializer.data
+    }
+    return Response(response_data, status=status.HTTP_200_OK)
+
+# 조회시 count api
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def count_view(request, popupId):
+    
+    option = request.GET.get('option')
+    
+    match option:
+        
+        case "popup":
+            data_query = OfflinePopup.objects.get(id=popupId)
+        case "place":
+            data_query = Place.objects.get(id=popupId)
+            
+    data_query.viewCount = data_query.viewCount + 1
+    data_query.save()  # 변경된 내용을 저장
+    
+    return Response(status=status.HTTP_200_OK)
+
+
         
 
 
